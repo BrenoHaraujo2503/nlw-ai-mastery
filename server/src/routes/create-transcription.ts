f83ecddc93de509df5d9c1d274a8ba829736daba
brenoharaujo2503 <brenoharaujomorais@gmail.com>
2023-09-12 21:39:34 -0300

    lint(server): create eslint config(rocketseat)

--- server/src/routes/create-transcription.ts
+++ server/src/routes/create-transcription.ts
@@ -1,27 +1,27 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance } from 'fastify'
 import { createReadStream } from 'node:fs'
 import { z } from 'zod'
-import { prisma } from "../lib/prisma";
-import { openai } from "../lib/openai";
+import { prisma } from '../lib/prisma'
+import { openai } from '../lib/openai'
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
   app.post('/videos/:videoId/transcription', async (request) => {
     const paramsSchema = z.object({
-      videoId: z.string().uuid()
+      videoId: z.string().uuid(),
     })
 
     const { videoId } = paramsSchema.parse(request.params)
 
     const bodySchema = z.object({
-      prompt: z.string()
+      prompt: z.string(),
     })
 
     const { prompt } = bodySchema.parse(request.body)
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
-        id: videoId
-      }
+        id: videoId,
+      },
     })
 
     const videoPath = video.path
@@ -33,18 +33,18 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
       language: 'pt',
       response_format: 'json',
       temperature: 0,
-      prompt
+      prompt,
     })
 
     const transcription = response.text
 
     await prisma.video.update({
       where: {
-        id: videoId
+        id: videoId,
       },
       data: {
-        transcription
-      }
+        transcription,
+      },
     })
     return transcription
   })
